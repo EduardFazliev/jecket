69b6cea5e7c85a489d36790bdb4d62d45bbcf6bb
efazliev <efazliev@infotech.team>
2016-06-27 18:46:34 +0300

    Add logger for build_step.

--- pull_request_build_step.py
+++ pull_request_build_step.py
@@ -1,6 +1,7 @@
 import os
 from subprocess import Popen, PIPE
 
+from jbi_logger import log
 from pull_request_file_comments import SendResultsToPullRequestFiles
 from pull_request_main_comments_section import PullRequestCommits, \
     SendResultsToPullRequest
@@ -13,9 +14,11 @@ passwd = 'jenkins'
 
 
 def execute_linux_command(cmd):
+    log('Executing command {}'.format(cmd))
     proc = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
     out, err = proc.communicate()
     code = proc.returncode
+    log('Command code"{0}, result"{1}, error:{2}'.format(code, out, err))
     if code != 0:
         return code, err
     elif code == 0:
@@ -23,22 +26,24 @@ def execute_linux_command(cmd):
 
 
 def send_file_results(file, pmd, checkstyle):
+    log('Sending results for file {}.'.format(file))
     file_comments = SendResultsToPullRequestFiles(
         base_api_link=base_api_link,
         checked_file=file,
         username=user,
         passwd=passwd
     )
-    print file_comments.send_static_check_results(pmd, checkstyle)
+    log('Sending results finished. Output: {}'.format(file_comments))
 
 
-def static_check(file, cmd, report_flag):
+def static_check(file, cmd, report_flag, type):
     code, result = execute_linux_command(cmd)
     if code != 0:
         count = 'Error while executing static check: {}'.format(result)
     else:
         i = 0
-        with open('{}_pmd.xml'.format(file), 'r') as f:
+        log('Trying to count errors in file {0}_{1}.xml'.format(file, type))
+        with open('{0}_{1}.xml'.format(file, type), 'r') as f:
             for line in f:
                 if report_flag in line: i += 1
         count = i
@@ -52,15 +57,16 @@ def commit_files_handler(commit_id):
     for file in files:
         if not file or '.java' not in file:
             continue
+        log('Checking file {}'.format(file))
         pmd_rules = os.environ.get("PMD_RULES", "java-codesize,java-empty,java-imports,java-strings")
         cmd = 'pmd/bin/run.sh pmd -l java --failOnViolation false -f xml -r {0}_pmd.xml -d {0} -R {1}'.format(file, pmd_rules)
         violations = '</violation>'
-        pmd_count = static_check(file, cmd, violations)
+        pmd_count = static_check(file, cmd, violations, 'pmd')
 
         checkstyle_rules = os.environ.get("CHECKSTYLE_RULES", './google_checks.xml')
         cmd = 'java -jar checkstyle.jar -f xml -o {0}_checkstyle.xml -c {1} {0}'.format(file, checkstyle_rules)
         violations = '<error'
-        checkstyle_count = static_check(file, cmd, violations)
+        checkstyle_count = static_check(file, cmd, violations, 'checkstyle')
 
         send_file_results(file, pmd_count, checkstyle_count)
 
