87fa2b80e12a0ed3edc8e09bfbdd885a1eb63a5c
EduardFazliev <napalmedd@gmail.com>
2016-07-21 18:52:32 +0300

    Fix file open mode.

--- jecket/set_conf.py
+++ jecket/set_conf.py
@@ -2,7 +2,7 @@ conf_file = '/tmp/jecket/jecket.conf'
 
 
 def main(args):
-    with open(conf_file, 'w') as f:
+    with open(conf_file, 'w+') as f:
         f.write('{0}\n'
                 '{1}\n'
                 '{2}\n'.format(args.base_link, args.username, args.password))
